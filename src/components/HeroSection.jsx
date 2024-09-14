a13e4f38f33ac25248bfb9acfc64620b66c6b36a
unknown <ngoumkwa.kenne@gmail.com>
2024-09-14 16:37:15 +0100

    QrCode component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -36,7 +36,7 @@ useEffect(()=>{
   
 >
   <span
-    className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
+    className="absolute inset-0  translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
   ></span>
 
   <span className="relative block border border-current bg-white px-8 py-3"> En Savoir Plus </span>
